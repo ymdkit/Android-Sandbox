600c0cfed141f1c030eaa2a307bc9d6ef5a88df6
ymdkit <yamada2021@s.okayama-u.ac.jp>
2022-04-17 10:21:59 +0900

    feat: add helper method

--- domain/src/main/java/com/example/domain/model/Memo.kt
+++ domain/src/main/java/com/example/domain/model/Memo.kt
@@ -3,4 +3,8 @@ package com.example.domain.model
 data class Memo(
     val id: String,
     val title: String
-)
+) {
+    companion object {
+        fun createSampleObject() = Memo(id = "1", title = "サンプル")
+    }
+}
