26b840a3e3bd1bd76988d0dc730bf0654e9239df
ymdkit <yamada2021@s.okayama-u.ac.jp>
2022-04-17 10:08:37 +0900

    feat: create memo repository

---
+++ domain/src/main/java/com/example/domain/model/Memo.kt
@@ -0,0 +1,6 @@
+package com.example.domain.model
+
+data class Memo(
+    val id: String,
+    val title: String
+)
---
+++ domain/src/main/java/com/example/domain/repository/MemoRepository.kt
@@ -0,0 +1,11 @@
+package com.example.domain.repository
+
+import com.example.domain.model.Memo
+
+interface MemoRepository {
+    suspend fun getMemoList(): List<Memo>
+    suspend fun getMemo(memoId: String): Memo
+    suspend fun createMemo(memo: Memo): Memo
+    suspend fun updateMemo(memo: Memo): Memo
+    suspend fun deleteMemo(memo: Memo)
+}
