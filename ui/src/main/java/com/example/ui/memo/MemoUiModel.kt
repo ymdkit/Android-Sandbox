649144e7a9eda1da5ccf327bfae565081a9e5913
ymdkit <yamada2021@s.okayama-u.ac.jp>
2022-04-17 10:21:48 +0900

    feat: add convert method

--- ui/src/main/java/com/example/ui/memo/MemoUiModel.kt
+++ ui/src/main/java/com/example/ui/memo/MemoUiModel.kt
@@ -1,6 +1,15 @@
 package com.example.ui.memo
 
+import com.example.domain.model.Memo
+
 data class MemoUiModel(
     val id: String,
     val title: String
-)
+) {
+    companion object {
+        fun fromMemo(memo: Memo): MemoUiModel = MemoUiModel(
+            id = memo.id,
+            title = memo.title
+        )
+    }
+}
