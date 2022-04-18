ba337c3c2b95f1c52368ef9780c7fb722871b0e6
ymdkit <yamada2021@s.okayama-u.ac.jp>
2022-04-18 16:41:27 +0900

    feat: add deeplink

--- app/src/main/java/com/example/sandboxapplication/MainActivity.kt
+++ app/src/main/java/com/example/sandboxapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sandboxapplication
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.ui.NavHostScreen
@@ -10,8 +12,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val data: Uri? = intent?.data
+        println(data)
+
         setContent {
             NavHostScreen()
+        }
+        data?.let {
+            Toast.makeText(baseContext, "url: $data", Toast.LENGTH_LONG).show()
         }
     }
 }
